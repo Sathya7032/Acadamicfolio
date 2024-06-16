0c376f8008649b818e401f8d029be4d250db2a07
satyanarayana8179 <acadamicfolio@gmail.com>
2024-06-16 21:45:47 +0530

    latests121

--- Backend/settings.py
+++ Backend/settings.py
@@ -85,30 +85,10 @@ CORS_ALLOWED_ORIGINS = [
     "https://www.acadamicfolio.info",
 ]
 
-CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_ALL_ORIGINS = True  # Only for debugging, not for production
 
-CORS_ALLOW_METHODS = [
-    "DELETE",
-    "GET",
-    "OPTIONS",
-    "PATCH",
-    "POST",
-    "PUT",
-]
 
-CORS_ALLOW_HEADERS = [
-    "accept",
-    "accept-encoding",
-    "authorization",
-    "content-type",
-    "dnt",
-    "origin",
-    "user-agent",
-    "x-csrftoken",
-    "x-requested-with",
-]
 
-CORS_REPLACE_HTTPS_REFERER = True  # For debugging purposes
 
 
 
